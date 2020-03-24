482a3b5b93abdf37f8f6246a8b4ab6f41f572d06
Aty <aty99@yahoo.com>
2020-03-24 13:51:52 +0200

    Initial Commit

--- Side../ViewController.swift
+++ Side../ViewController.swift
@@ -9,12 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var bluerView: UIVisualEffectView!
+    
+    @IBOutlet var sideView: UIView!
+    @IBOutlet var viewConestrain: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        bluerView.layer.cornerRadius = 15
+        sideView.layer.shadowColor = UIColor.black.cgColor
+        sideView.layer.shadowOpacity = 0.8
+        sideView.layer.shadowOffset = CGSize(width: 5, height: 0)
+        viewConestrain.constant = -175
+        
     }
 
-
-}
-
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        
+        return .lightContent
+    }
+    
+    @IBAction func panPerformed(_ sender: UIPanGestureRecognizer) {
+        
+        if sender.state == .began || sender.state == .changed {
+            
+            let transltion = sender.translation(in: self.view).x
+            if transltion > 0 {
+                
+                if viewConestrain.constant < 20 {
+                    UIView.animate(withDuration: 0.2,animations:  {
+                                       self.viewConestrain.constant += transltion/10
+                                       self.view.layoutIfNeeded()
+                                   })
+                }
+                
+            } else {
+                if viewConestrain.constant > -175 {
+                UIView.animate(withDuration: 0.2,animations:  {
+                    self.viewConestrain.constant += transltion/10
+                    self.view.layoutIfNeeded()
+                })
+            
+            
+        } else if sender.state == .ended {
+            
+                    if viewConestrain.constant < -100 {
+                    UIView.animate(withDuration: 0.2,animations:  {
+                    self.viewConestrain.constant += transltion/10
+                    self.view.layoutIfNeeded()
+                                  })
+                    
+                    
+        }
+                    else {
+                        UIView.animate(withDuration: 0.2,animations:  {
+                        self.viewConestrain.constant += 0
+                        self.view.layoutIfNeeded()
+                                      })
+                        
+                    }}}}}}
